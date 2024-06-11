aba943003336f6dee777ecaff61b6796da8e492c
ouyangshaocong <ouyangshaocong@163.com>
2024-06-11 16:36:37 +0800

    默认llama

--- remote_config/et_service_util.py
+++ remote_config/et_service_util.py
@@ -29,7 +29,7 @@ async def llm_async(query, role_play, context, inst_text, max_num_sentence, repe
         "role_play": role_play,
         "context": context,
         "inst_text": inst_text,
-        "spc_type": 'llm_glm',      # 可以指定llm大模型类型(llm_llama, llm_glm)，不过每次切换都需要卸载/加载，尽量不要切换
+        "spc_type": 'llm_llama',      # 可以指定llm大模型类型(llm_llama, llm_glm)，不过每次切换都需要卸载/加载，尽量不要切换
         "max_num_sentence": max_num_sentence,
         "repetition_penalty": repetition_penalty
     }
